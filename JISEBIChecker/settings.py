13dab329cca78a37935ed44de383d6f1584f29dd
adishazzahra <adishaapaz@gmail.com>
2023-09-30 23:34:22 +0700

    lagi

--- JISEBIChecker/settings.py
+++ JISEBIChecker/settings.py
@@ -125,5 +125,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    [os.path.join(BASE_DIR, 'manuscripts/templates/static')]
 ]
+
+STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
