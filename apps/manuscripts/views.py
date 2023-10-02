816bc0c1d065f09e0930e2a84be993203256a34f
Thaofoo <mthafixh@gmail.com>
2023-10-02 11:28:09 +0700

    fix: readjustments

--- apps/manuscripts/views.py
+++ apps/manuscripts/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 def upload(request):
-    return render(request, 'upload.html')
+    return render(request, 'manuscripts/upload.html')
 
 def full_report(request):
-    return render(request, "full_report.html")
+    return render(request, "manuscripts/full_report.html")
 
